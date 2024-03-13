2a8299d31a270dfce8cf1faa39efba6908b487b1
Jake <jacob.andreacchio@gmail.com>
2024-03-13 00:42:46 -0400

    initialize mod using fabric's pre-built template

---
+++ src/main/java/cncallmek/fishbowl/FishbowlClient.java
@@ -0,0 +1,10 @@
+package cncallmek.fishbowl;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FishbowlClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/cncallmek/fishbowl/FishbowlDataGenerator.java
@@ -0,0 +1,11 @@
+package cncallmek.fishbowl;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FishbowlDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
