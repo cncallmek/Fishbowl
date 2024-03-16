628b122d4c9c91912b59e27a35ea6c458321bb3e
Jake <jacob.andreacchio@gmail.com>
2024-03-16 00:18:58 -0400

    fixes some errors

--- src/main/java/cncallmek/fishbowl/Fishbowl.java
+++ src/main/java/cncallmek/fishbowl/Fishbowl.java
@@ -1,6 +1,5 @@
 package cncallmek.fishbowl;
 
-import cncallmek.fishbowl.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -8,10 +7,10 @@ import org.slf4j.LoggerFactory;
 
 public class Fishbowl implements ModInitializer {
 	public static final String MOD_ID = "fishbowl";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		ModItems.registerModItems();
+		LOGGER.info("Hello Fabric world!");
 	}
 }
