9a4563b73bc588c9317bb56f664c2a5873d8cb79
Jake <jacob.andreacchio@gmail.com>
2024-05-10 23:20:22 -0400

    changes some wolf origin powers and idk what else

--- src/main/java/cncallmek/fishbowl/item/ModFoodComponents.java
+++ src/main/java/cncallmek/fishbowl/item/ModFoodComponents.java
@@ -1,7 +1,8 @@
 package cncallmek.fishbowl.item;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-  public static final FoodComponent BONE = new FoodComponent.Builder().hunger(0).meat().saturationModifier(0).build();
+  public static final FoodComponent BONE = new FoodComponent.Builder().hunger(0).meat().saturationModifier(0).alwaysEdible().build();
 }
